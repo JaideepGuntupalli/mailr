40ec96e748791adb461f435ad700fbd5b61b583e
Jaideep <guntupallijaideep@gmail.com>
2022-12-08 11:40:23 +0530

    cleanup: unwanted functions

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,8 @@
 import { router } from "../trpc";
-import { authRouter } from "./auth";
 import { mailRouter } from "./mail";
 
 export const appRouter = router({
   mail: mailRouter,
-  auth: authRouter,
 });
 
 // export type definition of API
